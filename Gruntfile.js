30305c7a445c97b559af07bc815cc4152a972aa6
Robin Parisi <parisi.robin@gmail.com>
2014-03-14 17:04:10 +0100

    premier commit

---
+++ Gruntfile.js
@@ -0,0 +1,55 @@
+module.exports = function(grunt) {
+
+    // 1. Configuration principale
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+
+        less: {
+            prod: {
+                options: {
+                    compress: false,
+                    sourceMap: true,
+                    sourceMapFilename: '/build/css/style.css.map',
+                    outputSourceFiles: true
+                },
+                files: {
+                    "build/css/style.css": "less/style.less"
+                }
+            }
+        },
+        autoprefixer: {
+            dev: {
+                options: {
+                    browsers: ['last 3 versions', '> 1%', 'ie 8'],
+                    diff: false
+                },
+                files: {
+                    'build/css/style.css': 'build/css/style.css'
+                }
+            }
+        },
+        watch: {
+            less: {
+                options: {
+                    livereload: true,
+                    files: ['build/css/*']
+                },
+                files: ['less/**'],
+                tasks: ['less', 'autoprefixer']
+            }
+        }
+    });
+
+    // 2. Plugins
+    grunt.loadNpmTasks('grunt-contrib-imagemin');
+    grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-autoprefixer');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+
+    // 3. Tâches à lancer par default
+    grunt.registerTask('default', ['less', 'autoprefixer']);
+
+
+};
